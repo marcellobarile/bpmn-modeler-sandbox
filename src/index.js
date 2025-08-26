c6c9297d9f39b589dd99f4408fa4a1c05fea027e
marcello.barile <marcello.barile@camunda.com>
2025-08-26 13:04:51 +0200

    chore: revamp env

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import "./styles.css";
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 
-import bpmnXML from "./diagram.bpmn";
-
 import BpmnModeler from "bpmn-js/lib/Modeler";
+import { diagram } from "./diagram";
 
 const modeler = (window.modeler = new BpmnModeler({
   container: "#canvas"
 }));
 
-modeler.importXML(bpmnXML).catch((err) => {
+modeler.importXML(diagram).catch((err) => {
   if (err) {
     console.log("BPMN diagram import failed", err);
   }
